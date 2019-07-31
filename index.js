7888b87f34f307c66f8419edc6878e2da11eae8e
Brian Arango <brianvictus@gmail.com>
2019-07-31 02:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const nestedTarget = () => {
 };
 
 const deepestChild = () => {
- return document.querySelector('#grand-node div div div div');
+  return document.querySelector('#grand-node div div div div');
 };
 
 const increaseRankBy = (n) => {
-   const increase = document.getElementById('app').querySelectorAll('ul.ranked-list');
-    for (let i = 0; i < increase.length; i++){
-     increase[i].innerHTML = ((parseInt(increase[i].innerHTML)) + n);
-     }
+  const increase = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < increase.length; i++) {
+    increase[i].innerHTML = ((parseInt(increase[i].innterHTML)) +n );
+  }
 };
